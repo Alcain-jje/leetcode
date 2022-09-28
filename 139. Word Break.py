8978fb7c8f5106eb057f3aa80afff2c9ddbdec90
jieun <ox804@naver.com>
2022-09-28 09:23:58 +0900

    Python

--- 139. Word Break.py
+++ 139. Word Break.py
@@ -1,15 +1,14 @@
 from typing import List
 
-
 class Solution:
     def wordBreak(self, s: str, wordDict: List[str]) -> bool:
-        for i in wordDict:
-            s=s.replace(i,"")
-        if not s:
-            return True
-        else:
-            return False
-
+        dp=[False]*(len(s)+1)
+        dp[0]=True
+        for i in range(len(s)):
+            for j in range(i,len(s)):
+                if dp[i] and s[i:j+1]:
+                    dp[j+1]=True
+        return dp[-1]
 
 s = "catsandog"
 wordDict =  ["cats","dog","sand","and","cat"]
